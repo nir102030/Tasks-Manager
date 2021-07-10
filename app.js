56d6660b9dc4c84315400450ac5fd4940a728ee3
nir102030 <nir102030@gmail.com>
2021-07-11 00:22:53 +0300

    adding task dragging functionallity

--- app.js
+++ app.js
@@ -184,3 +184,61 @@ document.getElementById("tasks-list").addEventListener("dblclick", (e) => {
 		e.target.parentNode.appendChild(checkElement);
 	}
 });
+
+//Task dragging functionality
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	function handleDragStart(e) {
+		this.style.opacity = "0.4";
+
+		dragSrcEl = this;
+
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/html", this.innerHTML);
+	}
+
+	function handleDragEnd(e) {
+		this.style.opacity = "1";
+
+		items.forEach(function (item) {
+			item.classList.remove("over");
+		});
+	}
+
+	function handleDragOver(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+
+		return false;
+	}
+
+	function handleDragEnter(e) {
+		this.classList.add("over");
+	}
+
+	function handleDragLeave(e) {
+		this.classList.remove("over");
+	}
+
+	function handleDrop(e) {
+		e.stopPropagation();
+
+		if (dragSrcEl !== this) {
+			dragSrcEl.innerHTML = this.innerHTML;
+			this.innerHTML = e.dataTransfer.getData("text/html");
+		}
+
+		return false;
+	}
+
+	let items = document.querySelectorAll(".tasks-list .list-item");
+	items.forEach(function (item) {
+		item.addEventListener("dragstart", handleDragStart, false);
+		item.addEventListener("dragover", handleDragOver, false);
+		item.addEventListener("dragenter", handleDragEnter, false);
+		item.addEventListener("dragleave", handleDragLeave, false);
+		item.addEventListener("dragend", handleDragEnd, false);
+		item.addEventListener("drop", handleDrop, false);
+	});
+});
